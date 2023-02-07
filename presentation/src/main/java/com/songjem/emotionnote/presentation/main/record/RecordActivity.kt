1e2702bab68d2f491c89ad79af1a18387fc55215
Song Jemin <jemin56@naver.com>
2023-02-07 23:47:38 +0900

    feat : isSecretMode 컬럼 추가로 인한 DB 버전 업그레이드 및 마이그레이션

--- presentation/src/main/java/com/songjem/emotionnote/presentation/main/record/RecordActivity.kt
+++ presentation/src/main/java/com/songjem/emotionnote/presentation/main/record/RecordActivity.kt
@@ -94,7 +94,7 @@ class RecordActivity : BaseActivity<ActivityRecordBinding>(R.layout.activity_rec
 
             if(emotionStatus != "No Data" && reportContent != null && positiveString != "No Data" && negativeString != "No Data" && neutralString != "No Data") {
                 val emotionReport = EmotionReportItem(targetDate = targetDate, reportContent = reportContent, emotionStatus = emotionStatus, positive = positiveString.toFloat(),
-                    negative = negativeString.toFloat(), neutral = neutralString.toFloat(), firstReportDate = currentDate, lastReportDate = currentDate)
+                    negative = negativeString.toFloat(), neutral = neutralString.toFloat(), firstReportDate = currentDate, lastReportDate = currentDate, isSecretMode = false)
 
                 Log.d("songjem", "INSERT, emotionReportItem = $emotionReport")
                 viewModel.insertEmotionReport(emotionReport)
